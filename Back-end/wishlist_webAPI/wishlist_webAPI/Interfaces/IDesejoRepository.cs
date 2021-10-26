d22b3af1294cfbb8fe3b52b678b3435db9056d7c
Guilherme Diniz <guicardosodiniz123@gmail.com>
2021-10-26 10:20:44 -0300

    Api quase finalizada

---
+++ Back-end/wishlist_webAPI/wishlist_webAPI/Interfaces/IDesejoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wishlist_webAPI.Domains;
+
+namespace wishlist_webAPI.Interfaces
+{
+    interface IDesejoRepository
+    {
+        List<Desejo> ListarTodos();
+        void Cadastrar(Desejo NovoDesejo);
+        void Deletar(int IdDeletar);
+    }
+}
---
+++ Back-end/wishlist_webAPI/wishlist_webAPI/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wishlist_webAPI.Domains;
+
+namespace wishlist_webAPI.Interfaces
+{
+    interface IUsuarioRepository
+    {
+        List<Usuario> ListarTodos();
+        void Cadastrar(Usuario NovoUsuario);
+        void Deletar(int IdDeletar);
+        Usuario Login(string email, string senha);
+    }
+}
